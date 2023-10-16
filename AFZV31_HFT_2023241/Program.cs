867bc65ed7abe24c166d0d2b74a12ccfb2bb9ac6
judit.barsony <BJ_AFZV31@stud.uni-obuda.hu>
2023-10-16 14:33:57 +0200

    Add project files.

---
+++ AFZV31_HFT_2023241/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AFZV31_HFT_2023241
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
